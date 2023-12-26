c3c3249a1b6ca732cbd48b63c8e8808c7cf7806d
madhushreesk <88674169+madhushreesk@users.noreply.github.com>
2023-12-27 00:03:54 +0530

    built login form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="absolute bg-gradient-to-b from-black w-full">
+      <img className="w-72 " src="/netflix-logo.png" alt="netflix-logo" />
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "./Header";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/563192ea-ac0e-4906-a865-ba9899ffafad/6b2842d1-2339-4f08-84f6-148e9fcbe01b/IN-en-20231218-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="backgroundImg"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
